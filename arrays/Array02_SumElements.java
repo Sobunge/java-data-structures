ab6cea026f13232faf3d023b209031e181b9cbd7
Samuel Obunge <samuelobunge@gmail.com>
2026-06-09 14:43:35 +0300

    Finding the sum of all elements on an array

---
+++ arrays/Array02_SumElements.java
@@ -0,0 +1,14 @@
+public class Array02_SumElements {
+
+    public static void main(String[] args) {
+
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        System.out.println("The sum of the elements in the array is:" + sum);
+    }
+}
