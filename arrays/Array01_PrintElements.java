e79076bdcd79a82c478a57c723ceb4bd86d6f4f8
Samuel Obunge <samuelobunge@gmail.com>
2026-06-09 14:43:19 +0300

    Creating an array and printing its elements

---
+++ arrays/Array01_PrintElements.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Array01_PrintElements {
+
+    public static void main(String[] args) {
+
+        // Initialization
+        int[] numbers;
+
+        // Declaration
+        numbers = new int[] { 1, 2, 3, 4, 5 };
+
+        // Print elements
+        System.out.println(Arrays.toString(numbers));
+    }
+}
