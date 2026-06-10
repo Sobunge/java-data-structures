fd7b1c1cef81439937c0a4f267562a33b0a423a0
Samuel Obunge <samuelobunge@gmail.com>
2026-06-10 10:04:01 +0300

    update

--- arrays/Array11_ReverseWithArrays.java
+++ arrays/Array11_ReverseWithArrays.java
@@ -1,18 +1,17 @@
 import java.util.Arrays;
 
 public class Array11_ReverseWithArrays {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
         // Declaring and initializing an array
-        int[] numbers = {1, 2, 3, 4, 5};
+        int[] numbers = { 1, 2, 3, 4, 5 };
 
         // Creating a temp variable
         int temp;
 
-        
-        for(int i = 0; i < numbers.length ; i++){
-            for(int j = i + 1; j < numbers.length; j++){
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
                 // Swapping the elements
                 temp = numbers[i];
                 numbers[i] = numbers[j];
