ebac091f4b592978e6029b2e40971b7608e0ba5f
Samuel Obunge <samuelobunge@gmail.com>
2026-06-17 13:00:39 +0300

    Sorting array elements from largest to smallest without an array

---
+++ arrays/Array13_BackSortWithoutArrays.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Array13_BackSortWithoutArrays {
+    public static void main(String[] args){
+        //Declaring and initializing the array
+        int[] numbers = {3,2,1,10,11,43,22,1,32,1,0};
+
+        //Printing the sorted array
+        System.out.println("Sorted array from largest to smallest: " + Arrays.toString(sortArrayLtoS(numbers)));
+    }
+
+    public static int[] sortArrayLtoS(int[] arr){
+
+        //Swapping elements to get the largest to smallest
+        for(int i=0; i< arr.length; i++){
+            for(int j=1; j< arr.length;j++){
+                if(arr[j] > arr[j-1]){
+                    //Declaring the temp value holder
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+}
